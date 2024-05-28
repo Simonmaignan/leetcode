58251a1ae0b8c6c18598046375060844cffce7f5
Simon Maignan <simonmaignan88@gmail.com>
2024-05-28 12:12:23 +0200

    474. Ones and Zeros bottom up

--- 474.ones-and-zeroes.py
+++ 474.ones-and-zeroes.py
@@ -3,12 +3,14 @@
 #
 # [474] Ones and Zeroes
 #
+
+# @lc code=start
+# from pprint import pprint
 from typing import List, Tuple
 
 
-# @lc code=start
 class Solution:
-    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+    def findMaxForm_(self, strs: List[str], m: int, n: int) -> int:
         def count_1s_and_0s(string: str) -> Tuple[int]:
             nb_1s, nb_0s = 0, 0
             for c in string:
@@ -51,6 +53,49 @@ class Solution:
             return largest_subset_size
 
         return dfs(0, 0, 0)
+
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        def count_1s_and_0s(string: str) -> Tuple[int]:
+            nb_1s, nb_0s = 0, 0
+            for c in string:
+                if c == "0":
+                    nb_0s += 1
+                else:
+                    nb_1s += 1
+
+            return nb_0s, nb_1s
+
+        # strs_01: List[Tuple[int]] = [count_1s_and_0s(string) for string in strs]
+        largest_subset_size = 0
+        dp: List[List[List[int]]] = [
+            [[0] * (n + 1) for _ in range(m + 1)] for _ in range(len(strs) + 1)
+        ]
+
+        for i_s, string in enumerate(strs):
+            nb_0s, nb_1s = count_1s_and_0s(string)
+            # print(f"str={strs[i_s]} ({(nb_0s, nb_1s)})")
+            for i_0 in range(m + 1):
+                # print(f"i_0={i_0}")
+                for i_1 in range(n + 1):
+                    # print(f"i_1={i_1}")
+                    # print(f"dp[{i_s + 1}][{i_0}][{i_1}]=max()")
+                    # print(f"dp[{i_s}][{i_0}][{i_1}]={dp[i_s][i_0][i_1]}")
+                    dp[i_s + 1][i_0][i_1] = dp[i_s][i_0][i_1]
+                    if (i_0 - nb_0s) >= 0 and (i_1 - nb_1s) >= 0:
+                        # print(
+                        #     f"dp[{i_s}][{i_0 - nb_0s}][{i_1 - nb_1s}]+1={dp[i_s][i_0 - nb_0s][i_1 - nb_1s]+1}"
+                        # )
+                        dp[i_s + 1][i_0][i_1] = max(
+                            dp[i_s + 1][i_0][i_1], dp[i_s][i_0 - nb_0s][i_1 - nb_1s] + 1
+                        )
+                    # print(f"= {dp[i_s + 1][i_0][i_1]}")
+                    largest_subset_size = max(
+                        largest_subset_size, dp[i_s + 1][i_0][i_1]
+                    )
+
+        # pprint(dp)
+
+        return largest_subset_size
 
 
 # @lc code=end
