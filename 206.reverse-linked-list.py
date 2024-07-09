193474c6c9c1262a5aefc180eecfb114e3ceb0bd
Simon Maignan <simonmaignan88@gmail.com>
2024-07-09 16:39:30 +0200

    206 Reverse Linked List iterative

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -3,7 +3,7 @@
 #
 # [206] Reverse Linked List
 #
-from typing import Optional
+from typing import List, Optional
 
 
 class ListNode:
@@ -20,13 +20,29 @@ class ListNode:
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None:
+        # if head is None:
+        #     return None
+        # if head.next is None:
+        #     return head
+        # new_node = self.reverseList(head.next)
+        # new_node.next = head
+        # return new_node
+        nodes_list: List[ListNode] = []
+        while head:
+            nodes_list.append(head)
+            head = head.next
+
+        if not nodes_list:
             return None
-        if head.next is None:
-            return head
-        new_node = self.reverseList(head.next)
-        new_node.next = head
-        return new_node
+
+        reversed_list_first: ListNode = nodes_list.pop()
+        reversed_list: ListNode = reversed_list_first
+        while nodes_list:
+            reversed_list.next = nodes_list.pop()
+            reversed_list = reversed_list.next
+        reversed_list.next = None
+
+        return reversed_list_first
 
 
 # @lc code=end
