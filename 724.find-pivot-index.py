4d04e00f130a9d8826c300520064712a5b425840
Simon Maignan <simonmaignan88@gmail.com>
2024-07-10 12:01:32 +0200

    724 Find Pivot Index; Faster solution

--- 724.find-pivot-index.py
+++ 724.find-pivot-index.py
@@ -9,22 +9,13 @@ from typing import List
 # @lc code=start
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        n = len(nums)
-        prefixes: List[int] = [0] * (n + 1)
-        suffixes: List[int] = [0] * (n + 1)
+        left = 0
+        right = sum(nums)
         for i, num in enumerate(nums):
-            prefixes[i + 1] = prefixes[i] + num
-
-        for i in range(n - 1, -1, -1):
-            suffixes[i] = suffixes[i + 1] + nums[i]
-
-        # print(prefixes)
-        # print(suffixes)
-
-        for i in range(n):
-            # print(f"i={i}; prefix={prefixes[i]}; suffix={suffixes[i + 1]}")
-            if prefixes[i] == suffixes[i + 1]:
+            right -= num
+            if right == left:
                 return i
+            left += num
 
         return -1
 
