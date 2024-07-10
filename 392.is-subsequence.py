2c904b536441819d384407c5f4c87bab9c79ff0a
Simon Maignan <simonmaignan88@gmail.com>
2024-07-10 11:00:01 +0200

    392. Is Subsequence

---
+++ 392.is-subsequence.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=392 lang=python3
+#
+# [392] Is Subsequence
+#
+
+
+# @lc code=start
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        p_s = p_t = 0
+        n_s = len(s)
+        n_t = len(t)
+        while p_s < n_s and p_t < n_t:
+            # print(f"s[{p_s}]={s[p_s]} - t[{p_t}]={t[p_t]}")
+            while p_t < n_t and s[p_s] != t[p_t]:
+                # print(f"s[{p_s}]={s[p_s]} - t[{p_t}]={t[p_t]}")
+                p_t += 1
+            if p_t < n_t:
+                p_s += 1
+                p_t += 1
+
+        return p_s == n_s
+
+
+# @lc code=end
