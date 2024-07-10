0468bf7f447da16bb95afea0b436d2573d330a9e
Simon Maignan <simonmaignan88@gmail.com>
2024-07-10 13:03:38 +0200

    933. Number of recent calls; Improved speed with queue

--- 933.number-of-recent-calls.py
+++ 933.number-of-recent-calls.py
@@ -3,23 +3,24 @@
 #
 # [933] Number of Recent Calls
 #
-from typing import List
 
 
 # @lc code=start
+from collections import deque
+
+
 class RecentCounter:
 
     def __init__(self):
-        self.call_queue: List[int] = []
+        self.call_queue = deque()
 
     def ping(self, t: int) -> int:
+        # print(f"ping {t}")
         self.call_queue.append(t)
-        nb_calls = 0
-        call_i = len(self.call_queue) - 1
-        while call_i >= 0 and self.call_queue[call_i] >= t - 3000:
-            nb_calls += 1
-            call_i -= 1
-        return nb_calls
+        while self.call_queue[0] < t - 3000:
+            self.call_queue.popleft()
+            # print(f"pop {self.call_queue.popleft()}")
+        return len(self.call_queue)
 
 
 # Your RecentCounter object will be instantiated and called as such:
