ae45953519adc2482cb8a96bf3068d07136e47a6
Simon Maignan <simonmaignan88@gmail.com>
2024-07-10 15:18:07 +0200

    215. Kth largest element in an Array

---
+++ 215.kth-largest-element-in-an-array.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=215 lang=python3
+#
+# [215] Kth Largest Element in an Array
+#
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        return nums[-k]
+
+
+# @lc code=end
