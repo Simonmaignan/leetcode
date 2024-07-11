04c90facc05e89414284b2aac6fa228c1fb6b10d
Simon Maignan <simonmaignan88@gmail.com>
2024-07-11 08:53:06 +0200

    151 Reverse Words in a String; 2 Pointers

--- 151.reverse-words-in-a-string.py
+++ 151.reverse-words-in-a-string.py
@@ -3,13 +3,25 @@
 #
 # [151] Reverse Words in a String
 #
+from typing import List
 
 
 # @lc code=start
 class Solution:
     def reverseWords(self, s: str) -> str:
-        split_s = s.split()
-        return " ".join(reversed(split_s))
+        right = len(s) - 1
+        reversed_words: List[str] = []
+        while right >= 0:
+            while right >= 0 and s[right] == " ":
+                right -= 1
+            if right < 0:
+                break
+            left = right - 1
+            while left >= 0 and s[left] != " ":
+                left -= 1
+            reversed_words.append(s[left + 1 : right + 1])
+            right = left - 1
+        return " ".join(reversed_words)
 
 
 # @lc code=end
