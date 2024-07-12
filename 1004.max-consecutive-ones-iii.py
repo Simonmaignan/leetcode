86765576e0a5b844c7b55315a8d18b474f0615d2
Simon Maignan <simonmaignan88@gmail.com>
2024-07-12 12:05:46 +0200

    1004. Max consecutive ones III: While loop changed for for loop

--- 1004.max-consecutive-ones-iii.py
+++ 1004.max-consecutive-ones-iii.py
@@ -9,26 +9,22 @@ from typing import List
 # @lc code=start
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        left = right = 0
+        left = 0
         max_ones = 0
-        while right < n:
-            # If right at 1
-            if nums[right]:
-                right += 1
-            # If right at 0, with some 0 flip left
-            elif k > 0:
-                right += 1
-                k -= 1
-            # If right at 0, without 0 flip left
-            else:
-                # Move left until next 0
-                while nums[left]:
+        for right, num in enumerate(nums):
+            # If right at 0
+            if not num:
+                # with some 0 flip left
+                if k > 0:
+                    k -= 1
+                # without 0 flip left
+                else:
+                    # Move left until next 0
+                    while nums[left]:
+                        left += 1
+                    # Move left and right one step
                     left += 1
-                # Move left and right one step
-                left += 1
-                right += 1
-            max_ones = max(max_ones, right - left)
+            max_ones = max(max_ones, right - left + 1)
         return max_ones
 
 
