0b7f0c5bae8bfad7f58a4abe00bcc4d0c7cf99e7
Simon Maignan <simonmaignan88@gmail.com>
2024-07-14 19:03:41 +0200

    435. Non overlapping intervals; Sort by end

--- 435.non-overlapping-intervals.py
+++ 435.non-overlapping-intervals.py
@@ -3,35 +3,21 @@
 #
 # [435] Non-overlapping Intervals
 #
-from typing import List, Set
+from typing import List
 
 
 # @lc code=start
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        n = len(intervals)
-        overlap_intervals: Set[int] = set()
-        cur_int = 0
-        while cur_int < n:
-            # print(f"cur={cur_int}")
-            overlap_int = cur_int + 1
-            # Next overlaps with cur
-            while (
-                overlap_int < n
-                and intervals[cur_int][1] > intervals[overlap_int][0]
-            ):
-                # print(f"overlap={overlap_int}")
-                if intervals[overlap_int][1] < intervals[cur_int][1]:
-                    cur_int = overlap_int
-                    # print(f"new cur={cur_int}")
-                overlap_intervals.add(overlap_int)
-                overlap_int += 1
-
-            cur_int += 1
-            while cur_int in overlap_intervals:
-                cur_int += 1
-        return len(overlap_intervals)
+        intervals.sort(key=lambda x: x[1])
+        nb_intervals = 0
+        biggest_end = -5 * 10**4 - 1
+        for interval in intervals:
+            if interval[0] < biggest_end:
+                nb_intervals += 1
+            else:
+                biggest_end = interval[1]
+        return nb_intervals
 
 
 # @lc code=end
