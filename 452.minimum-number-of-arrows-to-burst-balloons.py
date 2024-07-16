492089def02dae03a54dc004457a3c642b368aa7
Simon Maignan <simonmaignan88@gmail.com>
2024-07-16 12:57:24 +0200

    452. Min nb of Arrows to burst balloons

--- 452.minimum-number-of-arrows-to-burst-balloons.py
+++ 452.minimum-number-of-arrows-to-burst-balloons.py
@@ -9,19 +9,14 @@ from typing import List
 # @lc code=start
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key=lambda x: x[0])
-        n = len(points)
+        points.sort(key=lambda x: x[1])
         nb_arrows = 0
-        start_overlap = 0
-        end_overlaps = 0
-        while start_overlap < n:
-            while (
-                end_overlaps < n
-                and points[end_overlaps][0] <= points[start_overlap][1]
-            ):
-                end_overlaps += 1
-            nb_arrows += 1
-            start_overlap = end_overlaps
+        previous_arrow = points[0][0] - 1
+        for balloon_start, balloon_end in points:
+            if previous_arrow < balloon_start:
+                previous_arrow = balloon_end
+                nb_arrows += 1
+
         return nb_arrows
 
 
