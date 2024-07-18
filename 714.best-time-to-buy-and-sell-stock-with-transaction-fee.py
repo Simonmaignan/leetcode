34098b5ec265d9221e5adfcbf15745b90e731371
Simon Maignan <simonmaignan88@gmail.com>
2024-07-18 14:06:10 +0200

    714. Best time to buy and sell stock with fee; DP with 2 variables only instead of list

--- 714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -10,22 +10,19 @@ from math import inf
 # @lc code=start
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        n = len(prices)
-        # dp is my max profit for n days with or without any share bought
-        dp: List[List[int]] = [[0] * 2 for _ in range(n + 1)]
-        dp[0][1] = -inf
-        for day, price in enumerate(prices):
+        share = -inf
+        no_share = 0
+        for price in prices:
             # I have bought a share meaning
             # I buy, the one from today with my max profit from yesterday
             # or I keep the one I bought before yesterday)
-            dp[day + 1][1] = max(dp[day][1], dp[day][0] - price)
+            share = max(share, no_share - price)
             # I don't have any share meaning
             # I keep my profit from yesterday
             # or I sell the share I had before yesterday)
-            dp[day + 1][0] = max(dp[day][0], dp[day][1] + price - fee)
-        # print(dp)
+            no_share = max(no_share, share + price - fee)
 
-        return max(dp[-1][0], dp[-1][1])
+        return no_share
 
 
 # @lc code=end
