b3f63f237da3ee68bf8a949ab07bdd70e0c56654
Simon Maignan <simonmaignan88@gmail.com>
2024-07-22 17:06:10 +0200

    236. Lowest common ancestor of a BT

--- 236.lowest-common-ancestor-of-a-binary-tree.py
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -3,7 +3,7 @@
 #
 # [236] Lowest Common Ancestor of a Binary Tree
 #
-from typing import Dict
+from typing import List, Optional
 
 
 class TreeNode:
@@ -26,33 +26,44 @@ class Solution:
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
-        memo: Dict[int, bool] = {}
+        self.p_path: List[TreeNode] = []
+        self.q_path: List[TreeNode] = []
 
-        def isChild(root: "TreeNode", p: "TreeNode", q: "TreeNode") -> bool:
-            if root is None:
-                return False
-            if root.val in memo:
-                return memo[root.val]
-            if root.val == p.val or root.val == q.val:
-                return True
-            is_child = isChild(root.left, p, q) or isChild(root.right, p, q)
-            memo[root.val] = is_child
-            return is_child
+        def dfs(
+            node: Optional["TreeNode"], current_path: List["TreeNode"]
+        ) -> None:
+            if node is None:
+                return
+            # Both q and p have been found
+            if self.p_path and self.q_path:
+                return
 
-        if root is None:
-            return None
-        is_left_child = (
-            root.val == p.val or root.val == q.val or isChild(root.left, p, q)
-        )
-        is_right_child = (
-            root.val == p.val or root.val == q.val or isChild(root.right, p, q)
-        )
-        if is_left_child and is_right_child:
-            return root
-        if is_left_child:
-            return self.lowestCommonAncestor(root.left, p, q)
-        else:
-            return self.lowestCommonAncestor(root.right, p, q)
+            if node == p:
+                self.p_path = current_path[:]
+
+            if node == q:
+                self.q_path = current_path[:]
+
+            current_path.append(node.left)
+            dfs(node.left, current_path)
+            current_path.pop()
+
+            current_path.append(node.right)
+            dfs(node.right, current_path)
+            current_path.pop()
+
+        dfs(root, [root])
+
+        n_p = len(self.p_path)
+        n_q = len(self.q_path)
+        i = max(n_p, n_q) - 1
+        while i >= 0:
+            if i >= n_p or i >= n_q:
+                i -= 1
+            elif self.p_path[i] == self.q_path[i]:
+                return self.p_path[i]
+            else:
+                i -= 1
 
 
 # @lc code=end
