e06a1dd6a7d6f079861cf560e477031cb0c0641f
Simon Maignan <simonmaignan88@gmail.com>
2024-07-26 16:09:16 +0200

    146. LRU Cache

--- 146.lru-cache.py
+++ 146.lru-cache.py
@@ -8,35 +8,32 @@ from typing import Dict
 
 # @lc code=start
 class DoubleLinkedNode:
-    def __init__(self, val) -> None:
+    def __init__(self, key, val) -> None:
+        self.key = key
         self.val = val
         self.prev: DoubleLinkedNode = None
         self.next: DoubleLinkedNode = None
 
     def __str__(self) -> str:
-        return f"val={self.val} - next={self.next}"
+        return f"key={self.key}; val={self.val}; next={self.next}"
 
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache: Dict[int, int] = {}
-        self.least: DoubleLinkedNode = DoubleLinkedNode(-1)
-        self.last: DoubleLinkedNode = DoubleLinkedNode(-1)
+        self.cache: Dict[int, DoubleLinkedNode] = {}
+        self.least: DoubleLinkedNode = DoubleLinkedNode(-1, -1)
+        self.last: DoubleLinkedNode = DoubleLinkedNode(-1, -1)
         self.least.next = self.last
         self.last.prev = self.least
         self.capacity: int = capacity
 
     def _move_to_last(self, key: int) -> None:
-        cur: DoubleLinkedNode = self.least
-        while cur.val != key:
-            cur = cur.next
-
         # Unlink cur node from where it is
-        cur.prev.next = cur.next
-        cur.next.prev = cur.prev
+        self.cache[key].prev.next = self.cache[key].next
+        self.cache[key].next.prev = self.cache[key].prev
 
-        self._add_before_last(cur)
+        self._add_before_last(self.cache[key])
 
     def _add_before_last(self, node: DoubleLinkedNode) -> None:
         # Add cur node just before last
@@ -48,27 +45,26 @@ class LRUCache:
     def get(self, key: int) -> int:
         if key in self.cache:
             self._move_to_last(key)
-            return self.cache[key]
+            return self.cache[key].val
 
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self._move_to_last(key)
+            self.cache[key].val = value
         else:
-            node_to_add = DoubleLinkedNode(key)
+            node_to_add = DoubleLinkedNode(key, value)
             self._add_before_last(node_to_add)
+            self.cache[key] = node_to_add
             # Over capacity after adding new node
-            if len(self.cache) == self.capacity:
+            if len(self.cache) > self.capacity:
                 # Remove least used from cache and linked list
                 to_remove: DoubleLinkedNode = self.least.next
-                del self.cache[to_remove.val]
                 # Replace least to point to new least
                 self.least.next = to_remove.next
                 to_remove.next.prev = self.least
-                del to_remove
-
-        self.cache[key] = value
+                del self.cache[to_remove.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
