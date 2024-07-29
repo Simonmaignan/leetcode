b5129397fef7eb07ca217df303556d726d77335f
Simon Maignan <simonmaignan88@gmail.com>
2024-07-29 12:12:23 +0200

    54. Spiral Matrix; Space optimization

--- 54.spiral-matrix.py
+++ 54.spiral-matrix.py
@@ -15,15 +15,14 @@ class Solution:
         m, n = len(matrix), len(matrix[0])
         spiral_order: List[int] = []
         directions: List[Tuple[int]] = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        visited: List[List[bool]] = [[False] * n for _ in range(m)]
-
+        MIN_NUM = -101
         i = j = k = 0
         while len(spiral_order) < (m * n):
             # print(spiral_order)
-            while 0 <= i < m and 0 <= j < n and not visited[i][j]:
+            while 0 <= i < m and 0 <= j < n and matrix[i][j] > MIN_NUM:
                 # print(f"i={i}; j={j}; k={k}")
                 spiral_order.append(matrix[i][j])
-                visited[i][j] = True
+                matrix[i][j] = MIN_NUM
                 i += directions[k][0]
                 j += directions[k][1]
                 # print(spiral_order)
