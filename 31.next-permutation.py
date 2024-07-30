02287e633719768fbdf47c6d6291211dc3433a08
Simon Maignan <simonmaignan88@gmail.com>
2024-07-30 09:12:15 +0200

    31. Next Permutation; Success

--- 31.next-permutation.py
+++ 31.next-permutation.py
@@ -3,7 +3,7 @@
 #
 # [31] Next Permutation
 #
-from typing import List, Tuple
+from typing import List
 
 
 # @lc code=start
@@ -14,21 +14,32 @@ class Solution:
         """
         n = len(nums)
 
-        def dfs(start_index: int) -> Tuple[int, int]:
-            if start_index < 0:
-                return -1, -1
-            print(f"start_index={start_index}")
-            for i in range(start_index - 1, -1, -1):
-                print(f"i={i}")
-                if nums[start_index] > nums[i]:
-                    return start_index, i
-            return dfs(start_index - 1)
+        # Find first number from the end lower than its follower
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
+            i -= 1
 
-        idx_1, idx_2 = dfs(n - 1)
-        if (idx_1, idx_2) == (-1, -1):
-            nums.sort()
-        else:
-            nums[idx_1], nums[idx_2] = nums[idx_2], nums[idx_1]
+        # print(f"i={i}")
+        if i >= 0:
+            # Find smallest number higher than nums[i] between i+1 and n
+            j = i + 1
+            smallest = nums[j]
+            for k in range(i + 2, n):
+                # print(f"k={k}; num={nums[k]}; j={j}; smallest={smallest}")
+                if nums[i] < nums[k] <= smallest:
+                    j = k
+                    smallest = nums[k]
+            # print(f"j={j}")
+            # Swap i and j
+            nums[i], nums[j] = nums[j], nums[i]
+        # print(nums)
+        # Reverse nums[i+1:]
+        i += 1
+        j = n - 1
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
 
 
 # @lc code=end
