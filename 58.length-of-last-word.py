53cfb2d5c22880f62b67da39692cc03cef5149af
Simon Maignan <simonmaignan88@gmail.com>
2024-07-30 08:11:31 +0200

    58. Length of last word; 2 pointers

--- 58.length-of-last-word.py
+++ 58.length-of-last-word.py
@@ -8,7 +8,14 @@
 # @lc code=start
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
+        n = len(s)
+        right = n - 1
+        while s[right] == " ":
+            right -= 1
+        left = right
+        while left >= 0 and s[left] != " ":
+            left -= 1
+        return right - left
 
 
 # @lc code=end
