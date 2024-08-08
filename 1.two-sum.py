d57ef10ad17890e87af2b43b2902f91b90a0660b
Simon Maignan <simonmaignan88@gmail.com>
2024-08-08 14:11:55 +0200

    1. Two sum; Simplified algo

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -9,12 +9,13 @@ from typing import Dict, List
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hash_map: Dict[int, int] = {element: idx for idx, element in enumerate(nums)}
+        hash_map: Dict[int, int] = {}
         # print(hash_map)
         for i, element in enumerate(nums):
-            rest: int = target - element
-            if rest in hash_map and hash_map[rest] != i:
-                return [i, hash_map[rest]]
+            complement: int = target - element
+            if complement in hash_map:
+                return [i, hash_map[complement]]
+            hash_map[element] = i
 
 
 # @lc code=end
