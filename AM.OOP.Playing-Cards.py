671840fdddb253e13e3f6a3573d5490150d1f595
Simon Maignan <simonmaignan88@gmail.com>
2024-08-26 09:30:17 +0200

    AM OOP Playing Cards; Design changed to use inheritance and abstract class to prepare for joker

--- AM.OOP.Playing-Cards.py
+++ AM.OOP.Playing-Cards.py
@@ -1,51 +1,77 @@
 # AM OOP Design
 # Playing Card
 # https://algo.monster/problems/oop_playing_cards
+from abc import ABC, abstractmethod
 from typing import Dict, List
 
-CARD_VALUES: Dict[str, int] = {
-    "A": 1,
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "J": 11,
-    "Q": 12,
-    "K": 13,
-}
+
+class Card(ABC):
+    @property
+    @abstractmethod
+    def value(self) -> int:
+        """An abstract property representing the card value"""
+        raise NotImplementedError
+
+    def __lt__(self, other: "Card") -> bool:
+        return self.value < other.value
 
 
-class Card:
+class PlayingCard(Card):
+    """A sub class of Card representing a playing card with its suit and value"""
+
+    PLAYING_CARD_VALUES: Dict[str, int] = {
+        "A": 1,
+        **{str(i): i for i in range(2, 11)},
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+    }
+
     def __init__(self, suit: str, value: str) -> None:
-        self.suit: str = suit
-        self.value: str = value
+        self.__suit: str = suit
+        self.__value: str = value
+
+    @property
+    def value(self) -> int:
+        """A property representing the card value"""
+        return self.PLAYING_CARD_VALUES[self.__value]
+
+    @property
+    def suit(self) -> str:
+        """A property representing the card suit"""
+        return self.__suit
 
     def __str__(self) -> str:
-        return f"{self.value} of {self.suit}"
+        return f"{self.__value} of {self.__suit}"
 
     def __hash__(self) -> str:
         # necessary for instances to behave sanely in dicts and sets.
-        return hash((self.suit, self.value))
+        return hash((self.__suit, self.__value))
 
-    def __eq__(self, other: "Card") -> bool:
+    def __eq__(self, other: "PlayingCard") -> bool:
         return self.suit == other.suit and self.value == other.value
-
-    def __lt__(self, other: "Card") -> bool:
-        return CARD_VALUES[self.value] < CARD_VALUES[other.value]
 
 
 class Game:
     def __init__(self) -> None:
-        self.card_deck: List[Card] = []
+        self.card_deck: List[PlayingCard] = []
 
     def add_card(self, suit: str, value: str) -> None:
-        """Create a new card object and add it to the Game card deck"""
-        self.card_deck.append(Card(suit=suit, value=value))
+        """Create a new card object and add it to the Game card deck
+
+        Args:
+            suit: the card suit
+            value: the card value
+        """
+        self.card_deck.append(PlayingCard(suit=suit, value=value))
+
+    def add_joker(self, color: str) -> None:
+        """Create a new joker card object and add it to the Game card deck
+
+        Args:
+            color: the joker card color
+        """
+        pass
 
     def card_string(self, card: int) -> str:
         """Returns the string representation of the card represented by card
@@ -66,3 +92,14 @@ class Game:
             True if card_a beats card_b, False otherwise
         """
         return self.card_deck[card_a] > self.card_deck[card_b]
+
+
+if __name__ == "__main__":
+    game = Game()
+    suit, value = input().split()
+    game.add_card(suit, value)
+    print(game.card_string(0))
+    suit, value = input().split()
+    game.add_card(suit, value)
+    print(game.card_string(1))
+    print("true" if game.card_beats(0, 1) else "false")
