34ce6f6a7d5a2b6994e545faa0ea0c24ea9b4ac9
Simon Maignan <simonmaignan88@gmail.com>
2024-09-05 11:18:21 +0200

    122. Best Time to Buy and Sell Stock II; Greedy

--- 122.best-time-to-buy-and-sell-stock-ii.py
+++ 122.best-time-to-buy-and-sell-stock-ii.py
@@ -10,19 +10,12 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp_buy: List[int] = [0] * n
-        dp_buy[0] = prices[0]
-        dp_sell: List[int] = [0] * n
+        total_profit = 0
         for i in range(1, n):
-            dp_buy[i] = max(dp_buy[i - 1], dp_sell[i - 1] - prices[i])
-            dp_sell[i] = max(
-                dp_sell[i - 1],
-                dp_buy[i] + prices[i],
-                dp_buy[i - 1] + prices[i],
-            )
-        # print(dp_buy)
-        # print(dp_sell)
-        return max(dp_buy[-1], dp_sell[-1])
+            buy_price = prices[i - 1]
+            sell_price = prices[i]
+            total_profit += max(0, sell_price - buy_price)
+        return total_profit
 
 
 # @lc code=end
